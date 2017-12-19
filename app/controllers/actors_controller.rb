457c717f47bf9f506dce399c12e3983e53d24d80
Mateo Olarte <mateo.olarte8@gmail.com>
2017-12-19 14:31:38 -0500

    [add] create actors feature

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,4 +2,23 @@ class ActorsController < ApplicationController
   def index
     @actors = Actor.all
   end
+
+  def new
+    @actor = Actor.new
+  end
+  
+  def create
+    @actor = Actor.new(actor_params)
+
+    if @actor.save
+      redirect_to actors_path, notice: "Se creo el actor correctamente"
+    else
+      render :new  
+    end  
+  end
+
+  private
+    def actor_params
+      params.require(:actor).permit(:name, :image_url, :birth_date, :birth_place, :bio, :alive, :death_date, :death_place)
+    end
 end
